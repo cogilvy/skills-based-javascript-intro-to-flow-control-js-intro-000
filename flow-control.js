3d0936c0f6ed67e7dcdc6aa9ffb8c38d73043da3
diligent-utility-1811 <chrisogilvy@cox.net>
2019-02-09 01:28:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,11 +26,12 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  
 switch(age) {
   case "(age >= 13 && age <= 19)" :
-    var = "You are a teenager"
+    value = "You are a teenager"
     break;
   default :
-    var = "You have an age"
+    value = "You have an age"
 }
 }
